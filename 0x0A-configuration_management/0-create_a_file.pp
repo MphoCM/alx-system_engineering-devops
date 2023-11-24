cd8e50218398556911eb32129d9116060a7c81dc
MphoCM <christian.mofokeng@gmail.com>
2023-11-24 14:00:52 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Puppet Manifest to Create and COnfigure a File
 
 file{ '/tmp/school':
-ensure => 'file',
-mode   => '0744',
-owner  => 'www-data',
-group  => 'www-data',
-content=> 'I love Puppet',
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 
 }
