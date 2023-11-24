63bfbb1f69aa11f532d3779cc322448c037e2174
MphoCM <christian.mofokeng@gmail.com>
2023-11-24 13:33:53 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Puppet Manifest to install from pip3.
 
-package { 'flask':
-  ensure   => '2.1.0', # Specify the desird version of Flask.
+package { 'werkzueg':
+  ensure   => '2.1.1', # Specify the desird version of Flask.
   provider => 'pip3',  # Use pip3 as the package provider.
 }
